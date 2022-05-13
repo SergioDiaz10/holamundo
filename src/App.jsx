7276a7abed02333178bd31f928661e890fd320ce
miriam isela flores antonio <isc18350307@gmail.com>
2022-05-13 10:33:34 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import saludo from "./saludo"
+function App() {
+  return (
+    <div className="App">
+      <saludo/>                       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Saludos.jsx
@@ -0,0 +1,7 @@
+const Saludo= ()=>{
+    returnt (
+        <h1>hola buen dia</h1>
+    )
+}
+
+export default Saludo;
